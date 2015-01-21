33fd6bfea85107af3840b4b5fc00dcfda6b7e41f
Rajeev Jorapur <rajeev.jorapur@gmail.com>
2015-01-21 16:19:27 +0530

    Assignment 2
    wrote code for makeCacheMatrix and cacheSolve matrices. Ran tests and
    seem to be working fine for 2x2 and 3x3 matrices

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix creates a matrix whose inverse is to be calculated
+## set -> sets the matrix
+## get -> gets the matrix
+## setsolve -> sets the value of the inverse of the matrix
+## getsolve -> gets the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+		
+	}
+	
+	get <- function() x
+	setsolve <- function(solve) m <<- solve
+	getsolve <- function() m
+	list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 
 }
 
@@ -12,4 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getsolve()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <-x$get()
+	m <- solve(data, ...)
+	x$setsolve(m)
+	m
+
 }
